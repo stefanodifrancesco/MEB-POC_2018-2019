6d283bd4501acd44a44b763f448e7b4a1791aa9a
naraku <naraku7686@gmail.com>
2019-02-03 14:36:33 +0100

    Update customQuery.php

--- Report tool/customQuery.php
+++ Report tool/customQuery.php
@@ -33,10 +33,10 @@ function FetchRow_CustomAssociate($statement, $metadata_arr){
         $row[$TableAndColumn] = $v;}
 
     return [$row, $metadata_arr];}
-
+var_dump($_POST);
 if(isset($_POST['queryStr'])) $sql = $_POST['queryStr']; else {
     echo
-        '<form action="/customQuery.php">'.
+        '<form action="/customQuery.php" method="post">'.
             'Query:<br>'.
             '<input type="text" name="queryStr" value="SELECT COUNT(*) FROM analytics_database.aggregateddata WHERE (TIMESTAMPDIFF(MINUTE, HoldEndDateTime, NOW()) <= 30)">'.
             '<br><br>'.
